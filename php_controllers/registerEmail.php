b11559f3efa252e4c0cca6d6eb5aa84c907d6692
Hit Bhalodia <hitkumar.bhalodia105373@marwadiuniversity.ac.in>
2021-04-30 12:50:35 +0530

    Done Verify Email From the Email

--- php_controllers/registerEmail.php
+++ php_controllers/registerEmail.php
@@ -1,33 +1,33 @@
 <?php
-	require 'connection_db.php';
+    require 'connection_db.php';
 
-	if(isset($_POST['register'])){
-		$is_veri = 0;
-		$is_sub = 0;
-		$query_register  = "INSERT INTO `visitors_email` (email,is_verified,is_subscribed) VALUES ('".$_POST['email']."','".$is_veri."','".$is_sub."')";
+    if(isset($_POST['register'])){
+        $is_veri = 0;
+        $is_sub = 0;
+        $query_register  = "INSERT INTO `visitors_email` (email,is_verified,is_subscribed) VALUES ('".$_POST['email']."','".$is_veri."','".$is_sub."')";
 
-		if($email_registerd = mysqli_query($con,$query_register))
+        if($email_registerd = mysqli_query($con,$query_register))
         {
             echo "You Had recived One verification Link to Your Email, Please verified the email to Get the FUNNY XKCD Images";
-
+            $email = $_POST['email'];
             $msg = "<html><head></head><body>";
-            $msg .= "<a href='abc.html'><h1>Verify Email : </h1></a>";
+            $msg .= "<a href='https://procit.ictmu.in/hit_rtCamp_xkcd/php_controllers/verify_email.php?email=".$email."'><h1>Verify Email</h1></a>";
             $msg.= "</body></html>";
             $subject_mail = "Verify Email - XKCD Images";
             $headers = "MIME-Version: 1.0" . "\r\n";
             $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-            $to = "hitkumar.bhalodia105373@marwadiuniversity.ac.in";
+            $to = $email;
             // More headers
             $headers .= 'From: <noreply@XKCD.in>' . "\r\n";
             if(mail($to,$subject_mail,$msg,$headers)){
-            	echo "<br>Done";
+                echo "<br>Done";
             }
             else{
-            	echo "<script>alert('Email Failed')</script>";
+                echo "<script>alert('Email Failed')</script>";
             }
         }
         else{
-        	echo "Email Already Registerd";
+            echo "Email Already Registerd";
         }
-	}
+    }
 ?>
---
+++ php_controllers/verify_email.php
@@ -0,0 +1,18 @@
+<?php
+	require 'connection_db.php';
+
+	if(isset($_GET['email'])){
+		$email_id = $_GET['email'];
+		$is_ver = 1;
+		$is_sub = 1;
+
+		$query = "UPDATE `visitors_email` SET is_verified='".$is_ver."' , is_subscribed = '".$is_sub."' WHERE email='".$email_id."' ";
+
+		if($email_registerd = mysqli_query($con,$query)){
+			echo "Email is Verified";
+		}
+		else{
+		    echo "Error";
+		}
+	}
+?>
